db1da2ac0cfa4267b10947c7e827a49a1ea053d6
zahra <zarahosseini43@gmail.com>
2022-10-23 01:14:49 +0330

    create a count task and  edit task

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,30 @@
 "use strict";
-let todoList;
+let todoList = [];
 
-let localList = localStorage.getItem("myList");
-localList = JSON.parse(localList);
-if (localList === null) {
-  todoList = [];
-} else {
-  todoList = localList;
-}
 let selectedDay = "shanbe";
 
-// ------------------------display saturday list when page is reloaded-------------
-const showSturdayList = () => {
-  let todoBoxes = document.querySelectorAll(".todo-box");
-  todoBoxes.forEach((item) => (item.style.display = "none"));
-  if (todoList.find((item) => item.day === selectedDay) === undefined) return;
-  let activeDay = todoList.find((item) => item.day === selectedDay).tasks;
-  for (let i = 0; i < activeDay.length; i++) {
-    let todoBox = document.querySelector(".todo-box").cloneNode(true);
-    todoBox.style.display = "flex";
-    todoBox.id = activeDay[i].id;
-    todoBox.querySelector("#user-task").innerText = activeDay[i].taskName;
-    todoBox.querySelector("#clock").innerText = activeDay[i].taskTime;
-
-    let mainTitle = document.querySelector(".main-title");
-    mainTitle.after(todoBox);
-  }
-}
-showSturdayList();
-
-
-// ------------------------getting task from user-------------
-
-// show todolist
+// ------------------------  update todo count-------------
+const updateNumber = (todoList) => {
+  let days = document.querySelectorAll(".day");
+  days.forEach((element) => {
+    let count = 0;
+    let temp = todoList.find((item) => item.day == element.id);
+    if (temp != undefined) {
+      count = temp.tasks.length;
+    }
+    element.querySelector(".task-num").innerText = count;
+  });
+};
+// -------------------------  show todolist  ------------------------
 const showList = () => {
+  let localList = localStorage.getItem("myList");
+  localList = JSON.parse(localList);
+  if (localList === null) {
+    todoList = [];
+  } else {
+    todoList = localList;
+  }
+  updateNumber(todoList);
   let todoBoxes = document.querySelectorAll(".todo-box");
   todoBoxes.forEach((item) => (item.style.display = "none"));
   if (todoList.find((item) => item.day === selectedDay) === undefined) return;
@@ -53,106 +44,143 @@ const showList = () => {
       element.remove();
     });
   }
+
+  const deleteBtns = document.querySelectorAll("#delete");
+  deleteBtns.forEach((element) => {
+    element.onclick = () => {
+      deleteFunction(element);
+    };
+  });
+
+  const editbtns = document.querySelectorAll("#edit");
+  editbtns.forEach((element) => {
+    element.onclick = () => {
+      editFunction(element);
+    };
+  });
 };
 
+showList();
 /// select days
 let days = document.querySelectorAll(".day");
 days.forEach((element) => {
   element.onclick = () => {
     selectedDay = element.id;
     showList();
-    const deletebtn = document.querySelectorAll("#delete");
-    deleteFunction(deletebtn, element);
   };
 });
-/// add task btn
+
+/// -----------------------  add task btn  -----------------
 const addBtn = document.querySelector("#add-btn");
 addBtn.onclick = () => {
   const task = document.querySelector("#task").value;
   const time = document.querySelector("#time").value;
-  save(task);
-  const newTask = {
-    id: Date.now(),
-    taskName: task,
-    taskTime: time,
-    isDone: false,
+  showSavePage(task);
+  const saveBtn = document.querySelector("#save-btn");
+  saveBtn.onclick = () => {
+    document.querySelector(".login-container").style.display = "none";
+    const newTask = {
+      id: Date.now(),
+      taskName: task,
+      taskTime: time,
+      isDone: false,
+    };
+    let tasksObject = {
+      day: "",
+      tasks: [],
+    };
+
+    if (todoList.find((item) => item.day === selectedDay) === undefined) {
+      tasksObject.tasks.push({ ...newTask });
+      tasksObject.day = selectedDay;
+      todoList.push({ ...tasksObject });
+    } else {
+      let temp = todoList.find((item) => item.day === selectedDay);
+      temp.tasks.push(newTask);
+    }
+    let wholeList = JSON.stringify(todoList);
+    localStorage.setItem("myList", wholeList);
+
+    document.querySelector("#task").value = "";
+    document.querySelector("#time").value = "";
+
+    showList();
   };
-  let tasksObject = {
-    day: "",
-    tasks: [],
+
+  const cancelBtn = document.querySelector("#cancel-btn");
+  cancelBtn.onclick = () => {
+    document.querySelector(".login-container").style.display = "none";
   };
-
-  if (todoList.find((item) => item.day === selectedDay) === undefined) {
-    tasksObject.tasks.push({ ...newTask });
-    tasksObject.day = selectedDay;
-    todoList.push({ ...tasksObject });
-  } else {
-    let temp = todoList.find((item) => item.day === selectedDay);
-    temp.tasks.push(newTask);
-  }
-  let wholeList = JSON.stringify(todoList);
-  localStorage.setItem("myList", wholeList);
-
-  document.querySelector("#task").value = "";
-  document.querySelector("#time").value = "";
-
-  showList();
 };
-
-// localStorage.clear()
 
 // --------------------------loginpage--------------
-const loginBtn = document.querySelector('#login-btn');
+const loginBtn = document.querySelector("#login-btn");
 loginBtn.onclick = () => {
-  const name = document.querySelector('#name').value;
-  const family = document.querySelector('#family').value;
-  const phone = document.querySelector('#phone').value;
+  const name = document.querySelector("#name").value;
+  const family = document.querySelector("#family").value;
+  const phone = document.querySelector("#phone").value;
   if (name.length === 0 || family.length === 0) {
-    alert('Please enter correct name')
+    alert("Please enter correct name");
   } else if (phone[0] != 0 || phone.length !== 11) {
-    alert('Invalid phone number')
+    alert("Invalid phone number");
   } else {
-    document.querySelector('.login-container').style.display = 'none'
+    document.querySelector(".login-container").style.display = "none";
+    document.getElementById("hello-text").innerText = "سلام " + name;
   }
-}
+};
 
 // --------------------------savepage--------------
-function save(task) {
-  document.querySelector('#save-text').placeholder = task;
-  document.querySelector('.login-container').style.display = 'flex';
-  document.querySelector('.login').style.display = 'none';
-  document.querySelector('.save').style.display = 'flex';
-  const saveBtn = document.querySelector('#save-btn');
-  saveBtn.onclick = () => {
-    document.querySelector('.login-container').style.display = 'none';
-  }
+function showSavePage(task) {
+  document.getElementById("save-text").value = task;
+  document.querySelector(".login-container").style.display = "flex";
+  document.querySelector(".login").style.display = "none";
+  document.querySelector(".save").style.display = "flex";
 }
 
 
-
-
-
-// ----------------------------------------
-
-function deleteFunction(deletebtn, day) {
-  deletebtn.forEach((element) => {
-    element.onclick = () => {
-      let choosenTaskId = (element.parentElement.parentElement.parentElement).id
-      const items = JSON.parse(localStorage.getItem('myList'));
-      let updatedItems = [];
-      for (let i = 0; i < items.length; i++) {
-        if (items[i].day === day.id) {
-          let tasks = items[i].tasks;
-          const filteredTasks = tasks.filter(item => item.id != choosenTaskId);
-          tasks = [...filteredTasks];
-          items[i].tasks = tasks;
-          updatedItems.push(items[i])
-        } else {
-          updatedItems.push(items[i])
-        }
-      }
-      localStorage.setItem('myList', JSON.stringify(updatedItems));
-      (element.parentElement.parentElement.parentElement).style.display = 'none';
+// --------------------------delete tasks--------------
+function deleteFunction(element) {
+  let choosenTaskId = element.parentElement.parentElement.parentElement.id;
+  const items = JSON.parse(localStorage.getItem("myList"));
+  let updatedItems = [];
+  for (let i = 0; i < items.length; i++) {
+    if (items[i].day == selectedDay) {
+      let tasks = items[i].tasks;
+      const filteredTasks = tasks.filter((item) => item.id != choosenTaskId);
+      tasks = [...filteredTasks];
+      items[i].tasks = tasks;
+      updatedItems.push(items[i]);
+    } else {
+      updatedItems.push(items[i]);
     }
-  })
-};
+  }
+  localStorage.setItem("myList", JSON.stringify(updatedItems));
+  showList();
+
+}
+
+// --------------------------edit tasks--------------
+function editFunction(element) {
+  let choosenTaskId = element.parentElement.parentElement.parentElement.id;
+  let todoList = JSON.parse(localStorage.getItem("myList"));
+  let temp = todoList.find((item) => item.day === selectedDay);
+  let task = temp.tasks.find((t) => t.id == choosenTaskId);
+  let index = temp.tasks.indexOf(task);
+  showSavePage(temp.tasks[index].taskName);
+
+  const saveBtn = document.querySelector("#save-btn");
+  saveBtn.onclick = () => {
+    document.querySelector(".login-container").style.display = "none";
+    let newTaskName = document.getElementById("save-text").value;
+    temp.tasks[index].taskName = newTaskName;
+    // console.log(newTaskName);
+    localStorage.setItem("myList", JSON.stringify(todoList));
+
+    showList();
+  };
+
+  const cancelBtn = document.querySelector("#cancel-btn");
+  cancelBtn.onclick = () => {
+    document.querySelector(".login-container").style.display = "none";
+  };
+}
